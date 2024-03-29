7fa514ef115e2ff514618d13b6dc15e458b57132
Mello Birkan <mgbirkan@icloud.com>
2024-03-29 00:46:32 -0300

    Initial Commit

---
+++ GuideBook/GuideBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideBookApp.swift
+//  GuideBook
+//
+//  Created by Marcello Gonzatto Birkan on 29/03/24.
+//
+
+import SwiftUI
+
+@main
+struct GuideBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
